4826de45018cd1e516a9caf98b6667dca23723d4
nvilla <natvnr@gmail.com>
2024-10-11 14:09:36 -0300

    Update get_prediction.py. NNclass added (but commented).

--- get_prediction.py
+++ get_prediction.py
@@ -1,10 +1,7 @@
 from NF import NF
 from NNgauss import NNgauss
-<<<<<<< HEAD
 # from NNclass import NNclass
-=======
-from NNclass import NNclass
->>>>>>> 5af7d3e (Initial commit)
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,15 +18,11 @@ input_data = dataset[input_props].to_numpy()
 # Models
 # the output shape is (n_samples, n_simulations, n_dimensions)
 nngauss = NNgauss(target_props, trial=45)
-<<<<<<< HEAD
 # nnclass = NNclass(target_props, trial=77)
-=======
-nnclass = NNclass(target_props, trial=77)
->>>>>>> 5af7d3e (Initial commit)
 nf = NF(target_props, trial=99)
 
 nngauss_pred = nngauss.get_sample(input_data, n_samples=10)
-nnclass_pred = nnclass.get_sample(input_data, 5, 2)
+# nnclass_pred = nnclass.get_sample(input_data, 5, 2)
 nf_pred = nf.get_sample(input_data, n_samples=10)
 
 print(nngauss_pred.shape)
@@ -40,7 +33,7 @@ print(nf_pred.shape)
 ytrue = dataset['smass']
 
 nngauss_ypred = nngauss_pred[0, :, 0]
-nnclass_ypred = nnclass_pred[0, :, 0]
+# nnclass_ypred = nnclass_pred[0, :, 0]
 nf_ypred = nf_pred[0, :, 0]
 
 
@@ -51,9 +44,9 @@ plt.plot([min(ytrue), max(ytrue)], [min(ytrue), max(ytrue)], ls='--', color='k')
 plt.scatter(ytrue, nngauss_ypred,
             color=nngauss.color, label=nngauss.label, marker=nngauss.marker,
             alpha=0.5)
-plt.scatter(ytrue, nnclass_ypred,
-            color=nnclass.color, label=nnclass.label, marker=nnclass.marker,
-            alpha=0.5)
+# plt.scatter(ytrue, nnclass_ypred,
+#             color=nnclass.color, label=nnclass.label, marker=nnclass.marker,
+#             alpha=0.5)
 plt.scatter(ytrue, nf_ypred,
             color=nf.color, label=nf.label, marker=nf.marker,
             alpha=0.5)
